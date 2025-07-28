8f08e7c37f16185b343bf7aee766d7f3d50d9fac
Ahmed Ismail <ahmedismailamer@gmail.com>
2025-07-28 17:26:54 +0300

    Initial commit for Streamfiy - setup project structure

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -54,7 +54,8 @@ export async function signup(req, res){
         res.cookie("jwt",token , {
             maxAge: 7 * 24 * 60 * 60 * 1000,
             httpOnly: true,
-            sameSite:"strict",
+            sameSite:"none",
+            // sameSite:"strict",
             secure:process.env.NODE_ENV === "production"
         })
         res.status(201).json({success:true , user:newUser})
@@ -87,7 +88,8 @@ export async function login(req, res){
         res.cookie("jwt",token , {
             maxAge: 7 * 24 * 60 * 60 * 1000,
             httpOnly: true,
-            sameSite:"strict",
+            // sameSite:"strict",
+            sameSite:"none",
             secure:process.env.NODE_ENV === "production"
         })
         res.status(200).json({success:true , user})
