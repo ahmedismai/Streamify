31360f88668832e106a06e1e17ece12790d4c4b7
Ahmed Ismail <ahmedismailamer@gmail.com>
2025-07-28 06:48:25 +0300

    Initial commit for Streamfiy - setup project structure

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,29 +1,40 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import express from "express"
+import authRouter from "./routes/auth.route.js"
+import userRouter from "./routes/user.route.js"
+import chatRouter from "./routes/chat.route.js"
+import "dotenv/config"
+import { connectDB } from "./lib/db.js"
+import cookieParser from "cookie-parser"
+import cors from "cors"
+import path from "node:path"
 
-dotenv.config();
-const app = express();
-app.use(cors());
-app.use(express.json());
+const app = express()
+const PORT = process.env.PORT || 3000
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname =path.resolve()
 
-app.get('/api/test', (req, res) => {
-  res.json({ message: 'API is working 🚀' });
-});
+app.use(cors({
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    credentials:true,
+}))
+app.use(express.json())
+app.use(cookieParser())
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../../Frontend/dist')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../Frontend/dist', 'index.html'));
-  });
-}
+app.use("/auth" , authRouter)
+app.use("/users" , userRouter)
+app.use("/chat" , chatRouter)
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+if (process.env.NODE_ENV === "production") {
+    const frontendPath = path.join(__dirname, "../Frontend/dist");
+    app.use(express.static(frontendPath));
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(frontendPath, "index.html"));
+    });
+  }
+  
+
+app.listen(PORT , ()=>{
+    console.log(`Server is running on port ${PORT}`)
+    connectDB();
+})
+ 
