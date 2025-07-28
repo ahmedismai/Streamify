71484f837d00b88bad64d25e0937755afc3a5b09
Ahmed Ismail <ahmedismailamer@gmail.com>
2025-07-28 09:26:16 +0300

    Initial commit for Streamfiy - setup project structure

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -2,7 +2,7 @@
 
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3000":"https://streamify-lemon.vercel.app"
+const BASE_URL = import.meta.env.VITE_API_URL
 export const axiosInstance = axios.create({
 baseURL: BASE_URL,
 withCredentials:true
