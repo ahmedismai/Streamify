2d68b9c86d2d8fc7fdd15a444576f8f86cc0b887
Ahmed Ismail <45186751+ahmedismai@users.noreply.github.com>
2025-07-28 08:27:28 +0300

    Update axios.js

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -3,7 +3,10 @@
 import axios from "axios";
 
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3000":""
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:3000"
+    : import.meta.env.VITE_API_URL;
 export const axiosInstance = axios.create({
     baseURL: BASE_URL, 
     withCredentials:true
