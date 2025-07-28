ccfc923f3d9334b8b554d86c6995e0db6b9bfab8
Ahmed Ismail <ahmedismailamer@gmail.com>
2025-07-28 10:16:51 +0300

    Initial commit for Streamfiy - setup project structure

---
+++ Backend/src/index.js
@@ -0,0 +1,46 @@
+import express from "express";
+import authRouter from "./routes/auth.route.js";
+import userRouter from "./routes/user.route.js";
+import chatRouter from "./routes/chat.route.js";
+import "dotenv/config";
+import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import path from "node:path";
+import { fileURLToPath } from "url";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://streamify-fsld.vercel.app"],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"]
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/auth", authRouter);
+app.use("/users", userRouter);
+app.use("/chat", chatRouter);
+
+app.get("/" , (req,res)=> {
+  return res.status(200).json("Hello World!")
+})
+if (process.env.NODE_ENV === "production") {
+  const frontendPath = path.join(__dirname, "../../frontend/dist");
+  app.use(express.static(frontendPath));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(frontendPath, "index.html"));
+  });
+}
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+  connectDB();
+});
