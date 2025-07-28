c395aa16616d83a19cdd48ffdd21aa6738f99ead
Ahmed Ismail <ahmedismailamer@gmail.com>
2025-07-28 09:44:21 +0300

    Initial commit for Streamfiy - setup project structure

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -15,23 +15,13 @@ const PORT = process.env.PORT || 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://streamify-fsld.vercel.app"
-];
-
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE"]
-}));
-
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    credentials: true,
+    methods:["GET", "POST", "PUT","DELETE"]
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
