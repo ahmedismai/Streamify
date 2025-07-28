e0a6f54c159e78cd9c6a04efc39ae2d643aca934
Ahmed Ismail <ahmedismailamer@gmail.com>
2025-07-28 06:52:59 +0300

    Initial commit for Streamfiy - setup project structure

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,40 +1,38 @@
-import express from "express"
-import authRouter from "./routes/auth.route.js"
-import userRouter from "./routes/user.route.js"
-import chatRouter from "./routes/chat.route.js"
-import "dotenv/config"
-import { connectDB } from "./lib/db.js"
-import cookieParser from "cookie-parser"
-import cors from "cors"
-import path from "node:path"
+import express from "express";
+import authRouter from "./routes/auth.route.js";
+import userRouter from "./routes/user.route.js";
+import chatRouter from "./routes/chat.route.js";
+import "dotenv/config";
+import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import path from "node:path";
 
-const app = express()
-const PORT = process.env.PORT || 3000
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-const __dirname =path.resolve()
+const __dirname = path.resolve();
 
 app.use(cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
-    credentials:true,
-}))
-app.use(express.json())
-app.use(cookieParser())
+  origin: process.env.CLIENT_URL || "http://localhost:5173",
+  credentials: true,
+}));
+app.use(express.json());
+app.use(cookieParser());
 
-app.use("/auth" , authRouter)
-app.use("/users" , userRouter)
-app.use("/chat" , chatRouter)
+app.use("/auth", authRouter);
+app.use("/users", userRouter);
+app.use("/chat", chatRouter);
 
 if (process.env.NODE_ENV === "production") {
-    const frontendPath = path.join(__dirname, "../Frontend/dist");
-    app.use(express.static(frontendPath));
-    app.get("*", (req, res) => {
-      res.sendFile(path.join(frontendPath, "index.html"));
-    });
-  }
-  
+  const frontendPath = path.join(__dirname, "dist");
+  app.use(express.static(frontendPath));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(frontendPath, "index.html"));
+  });
+}
 
-app.listen(PORT , ()=>{
-    console.log(`Server is running on port ${PORT}`)
-    connectDB();
-})
- 
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  connectDB();
+});
