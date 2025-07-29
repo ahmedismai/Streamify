bee59cfbee8c21a8e38ea90f5d32d3e1524bc637
Ahmed Ismail <ahmedismailamer@gmail.com>
2025-07-29 16:28:07 +0300

    Fixed Navbar

--- Frontend/src/Components/Navbar/Navbar.jsx
+++ Frontend/src/Components/Navbar/Navbar.jsx
@@ -25,36 +25,18 @@ export default function Navbar() {
         <div className='container mx-auto sm:px-6 lg:px-8'>
             <div className='flex items-center justify-end w-full'>
                 {/* logo only in the chat page  */}
-                {window.innerWidth < 640 ? (
-                    <>
-                    {isChatPage && (
-                        <div className='pl-5'>
-                        <Link to={"/"} className='flex items-center gap-2.5'>
-                            <ShipWheelIcon className='size-9 text-primary'/>
-                            <span className='text-md sm:text-3xl font-mono font-bold text-transparent 
-                                bg-gradient-to-r from-primary to-secondary bg-clip-text tracking-wider'>
-                                Streamify
-                            </span>
-                        </Link>
-                        </div>
-                    )}
-                    </>
-                ) : (
-                    <>
-                    {isChatPage && (
-                            <div className='pl-5'>
-                            <Link to={"/"} className='flex items-center gap-2.5'>
-                                <ShipWheelIcon className='size-9 text-primary'/>
-                                <span className='text-md sm:text-3xl font-mono font-bold text-transparent 
-                                    bg-gradient-to-r from-primary to-secondary bg-clip-text tracking-wider'>
-                                    Streamify
-                                </span>
-                            </Link>
-                            </div>
-                        )}
-                    </>
-                )}
                 
+                {isChatPage && (
+                <div className='pl-5'>
+                <Link to={"/"} className='flex items-center gap-2.5'>
+                    <ShipWheelIcon className='size-9 text-primary'/>
+                    <span className='text-md sm:text-3xl font-mono font-bold text-transparent 
+                        bg-gradient-to-r from-primary to-secondary bg-clip-text tracking-wider'>
+                        Streamify
+                    </span>
+                </Link>
+                </div>
+            )}
           
             <div className='flex items-center gap-2 sm:gap-4 ml-auto relative'>
                 <Link to={'/notifications'}>
@@ -80,4 +62,4 @@ export default function Navbar() {
         </div>
     </nav>
   )
-} 
+}
