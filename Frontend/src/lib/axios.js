d5c14166c7e9931d04c11bb82a597d16280799f6
Ahmed Ismail <ahmedismailamer@gmail.com>
2025-09-02 18:37:08 +0300

    Fixed api

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,9 +1,7 @@
-
-
 import axios from "axios";
 
-const BASE_URL = "https://streamify-production-27b0.up.railway.app"
+const BASE_URL = "https://streamify-4ov3.vercel.app";
 export const axiosInstance = axios.create({
-baseURL: BASE_URL,
-withCredentials:true
-})
+  baseURL: BASE_URL,
+  withCredentials: true,
+});
